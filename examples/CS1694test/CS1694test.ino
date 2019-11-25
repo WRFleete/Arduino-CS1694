bdf7560e1050247ff8565df0a8ce03c399165b6d
William <32031159+WRFleete@users.noreply.github.com>
2019-11-26 10:39:10 +1300

    Update CS1694test.ino

--- examples/CS1694test/CS1694test.ino
+++ examples/CS1694test/CS1694test.ino
@@ -19,64 +19,13 @@ void setup() {
 CSDisplayDriver.displaySetup(true); //my display is "7x10" mode, set false if display doesnt look right ("6x11" mode)
   CSDisplayDriver.setBrightness(7); //full brightness level
   CSDisplayDriver.clearDisplay(); // clear the display memory
-  // put your setup code here, to run once:
-
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-// byte inputButtons = CSDisplayDriver.readButtons();//read the button matrix
+
 CSDisplayDriver.convertVar(millis()); // convert a variable to 7 segment numbers for the display, takes unsigned long, shows first 6 MSD 
-/*if(bitRead(inputButtons,0)){
-  delay(60);
-   digitVal ++;
-  if(digitVal > 36){digitVal = 0;}
-  CSDisplayDriver.dispNum(digitVal,editDigit); 
-  //setBrightness(brightness);
-  }
 
-  if(bitRead(inputButtons,1)){
-    delay(60);
- digitVal --;
-  if(digitVal > 36){digitVal = 36;}
-  CSDisplayDriver.dispNum(digitVal,editDigit); 
-  }
-if(bitRead(inputButtons,2)){
-  delay(250);
-  editDigit --;
-  if(editDigit > 5){editDigit = 5;}}
-   //dispNum(brightness,7); //turn on the lamps in "digit" 7 counting in binary 0 - 7
-//dispNum(127,6); //turn on all the lamps 
-//dispNum(127,7); 
-
-  switch(editDigit){
-    case 0:
-    CSDisplayDriver.dispNum(1,6);
-     CSDisplayDriver.dispNum(0,7);
-    break;
-    case 1:
-    CSDisplayDriver.dispNum(2,6);
-     CSDisplayDriver.dispNum(0,7);
-    break;
-    case 2:
-    CSDisplayDriver.dispNum(8,6);
-     CSDisplayDriver.dispNum(0,7);
-    break;
-    case 3:
-    CSDisplayDriver.dispNum(16,6);
-     CSDisplayDriver.dispNum(0,7);
-    break;
-    case 4:
-     CSDisplayDriver.dispNum(0,6);
-    CSDisplayDriver.dispNum(1,7);
-    break;
-    case 5:
-     CSDisplayDriver.dispNum(0,6);
-    CSDisplayDriver.dispNum(2,7);
-    break;
-
-   
-    }*/
    
 
 CSDisplayDriver.updateDisplay(); //refresh the display, transferring the contents of the variable array into display memory on the chip
