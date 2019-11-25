210c13e3f78ac1b46be52906ae302bf5c3ccbc04
William <32031159+WRFleete@users.noreply.github.com>
2019-11-26 10:36:51 +1300

    Update CS1694test.ino
    adding comments

--- examples/CS1694test/CS1694test.ino
+++ examples/CS1694test/CS1694test.ino
@@ -7,17 +7,18 @@ first library I have written to some degree
 
 uses the 3 buttons on my board to change digits and scroll through the segment data for numbers and letters
 
-
+this test program just displays the millis() on the LED display (or the first 6 MSD if larger than 6 digits)
+none of the push buttons will respond
 
 */
 byte editDigit = 0;
 byte digitVal = 0;
-CSDisplayDriver CSDisplayDriver(12,11,8);
+CSDisplayDriver CSDisplayDriver(12,11,8); //data,clock,strobe pins
 void setup() {
 
-CSDisplayDriver.displaySetup(true);
-  CSDisplayDriver.setBrightness(7);
-  CSDisplayDriver.clearDisplay();
+CSDisplayDriver.displaySetup(true); //my display is "7x10" mode, set false if display doesnt look right ("6x11" mode)
+  CSDisplayDriver.setBrightness(7); //full brightness level
+  CSDisplayDriver.clearDisplay(); // clear the display memory
   // put your setup code here, to run once:
 
 }
